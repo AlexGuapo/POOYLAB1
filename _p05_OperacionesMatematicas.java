2996cd9efe99e4d7b8bd3e44a0b5a610a53ed64c
AfootZebra2255 <greyesduenas@gmail.com>
2023-08-22 13:28:13 -0600

    actividad2

--- _p05_OperacionesMatematicas.java
+++ _p05_OperacionesMatematicas.java
@@ -11,5 +11,11 @@ public class _p05_OperacionesMatematicas {
         div=x/y;
         rest=x%y;
         pot=Math.pow(x,y);
+
+        System.out.println(String.format("Los numeros son: %.2f, %.2f",x,y));
+        System.out.println("\nEl resultado de las operaciones es el siguiente:\n");
+        System.out.println(String.format("Suma: %.2f\nResta: %.2f",suma,resta));
+        System.out.println(String.format("Multiplicacion: %.2f\nDivision: %.2f",mult,div));
+        System.out.println(String.format("Residuo: %.2f\nPotencia: %.2f",rest,pot));
     }
 }
