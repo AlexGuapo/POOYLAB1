93445298577eedb0983e403510309e7eb0c94c8b
AfootZebra2255 <greyesduenas@gmail.com>
2023-08-29 15:47:52 -0600

    Actividad 5

--- _p26_TipoAngulo.java
+++ _p26_TipoAngulo.java
@@ -1,18 +1,28 @@
-// Muestra la estación del año que corresponde al número introducido
+// Muestra el tipo de angulo según los grados
 
 import java.util.Scanner;
 
 public class _p26_TipoAngulo {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.print("\033[H\033[2J"); System.out.flush();
-        System.out.println("Muestra la estacion del año en base al número:\n");
-        System.out.print("Dame un numero (1..4) ? ");
-        int n = new Scanner(System.in).nextInt();
+        System.out.println("Muestra el tipo de ángulo \n");
+        System.out.print("Dame un ángulo entre 0 y 360 grados? ");
+        int angulo = new Scanner(System.in).nextInt();
 
-        if(n==1) System.out.println("Primavera");
-        if(n==2) System.out.println("Verano");
-        if(n==3) System.out.println("Otoño");
-        if(n==4) System.out.println("Invierno");
+        System.out.print("El ángulo que introdujiste: ");
+        if(angulo<90)
+                System.out.println("Es agudo");
+        if(angulo==90)
+                System.out.println("Es recto");
+        if(angulo>90 && angulo<180)
+                System.out.println("Es obtuso");
+        if(angulo==180)
+                System.out.println("Es llano");
+        if(angulo>180 && angulo<360)
+                System.out.println("Es concavo");
+        if(angulo==360)
+
+        System.out.println("Es completo");
         System.out.println("\nProceso terminado ....");
     }
 }
